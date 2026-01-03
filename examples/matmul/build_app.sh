9747217c79e64c221a5eb1ecf115acf50539facb
Alwaysproblem <reganyang0415@gmail.com>
2026-01-03 04:26:02 +0000

    User the static cubin

--- examples/matmul/build_app.sh
+++ examples/matmul/build_app.sh
@@ -5,4 +5,5 @@ WORKDIR=`git rev-parse --show-toplevel`
 ${WORKDIR}/build/bin/cuda-tile-opt example.mlir --mlir-print-ir-after-all -cse
 
 ${WORKDIR}/build/bin/cuda-tile-translate example.mlir --bytecode-version=13.1 --mlir-to-cudatilebc --no-implicit-module -o example.tilebc
+tileiras --gpu-name sm_120 example.tilebc -o example.tilebc # (Optional: on the physical machine) please run this if you run on the gpu runtime container
 g++ example.cpp -o example -I/usr/local/cuda/include -L/usr/local/cuda/lib64 -lcuda
